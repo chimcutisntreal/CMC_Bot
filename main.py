34cbe8e4fdd2a869f1a088228bdd10ef5fec8335
Chu Triệu Chinh <43743965+chimcutisntreal@users.noreply.github.com>
2021-09-30 06:47:37 +0700

    update

--- main.py
+++ main.py
@@ -24,18 +24,23 @@ if __name__ == '__main__':
         @client.on(events.NewMessage(chats='CMC_fastest_alerts'))
         async def get_message(event):
             msg = event.raw_text
-            print(style.BLUE + msg)
             index_percentage = msg.find('%')
             index_address = msg.find('0x')
             is_first_pump = msg.find('first pump')
+            index_liquidity = msg.find('BNB')
+            if not index_liquidity:
+                index_liquidity = msg.find('BUSD')
+
             if index_percentage >= 0 and index_address >= 0 and is_first_pump >= 0:
                 percentage = int(msg[index_percentage-2:index_percentage].strip())
-                if percentage <= 11:
+                liquidity = float(msg[index_liquidity-8:index_liquidity].strip())
+                print(style.BLUE + currentTimeStamp + ' Received Token info ')
+                if percentage <= 8 and (50 <= liquidity <= 300 or 18000 <= liquidity <= 120000):
                     token_to_buy = msg[index_address:index_address + 42]
-                    print('Token to buy: ' + token_to_buy)
+                    print(currentTimeStamp + 'Token to buy: ' + token_to_buy)
                     execute().buy(web3, token_to_buy)
                 else:
-                    print('Buy/Sell Taxes are too high')
+                    print('Buy/Sell Taxes are too high or Liquidity is lower than 50 BNB')
 
         client.start()
         client.run_until_disconnected()
